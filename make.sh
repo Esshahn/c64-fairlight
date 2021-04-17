402506f3773a66b0c547d7ca33a51b78d0a2f3c1
Ingo Hinterding <ingo@hinterding.de>
2021-04-17 23:15:30 +0200

    basic code ready

--- make.sh
+++ make.sh
@@ -6,7 +6,7 @@
 #
 # -e or --emulator (optional)
 # which emulator to start after compilation
-# can be either none, "vice" or "64debugger"
+# can be either none or "vice" 
 # default: vice
 #
 # -f or --filename (optional)
@@ -25,7 +25,6 @@
 build_folder=build
 os=mac # mac, linux, win
 path_vice=/Applications/vice-gtk/bin
-path_64debugger=/Applications/c64-debugger/C64Debugger.app/Contents/MacOS/c64debugger
 path_acme=bin/$os/acme
 path_exomizer=bin/$os/exomizer
 
@@ -49,10 +48,6 @@ while true ; do
             d64=1
             shift 1
         ;;
-        -t|-tools )
-            tools=1
-            shift 1
-        ;;
         *)
             break
         ;;
@@ -63,21 +58,9 @@ done;
 [ ! $filename ] && filename="main"
 
 
-# create build folder if it is missing
-if [ ! -d $build_folder ]
-then
-    mkdir $build_folder
-    echo "build folder does not exist, creating it.."
-fi
-
-# execute custom tool
-if [ $tools ]
-then
-    echo rebuilding sprites from png image... 
-    echo
-    python3 tools/png2spd.py 'sources/sprites-001.png' 'gfx/sprites.spd'
-fi
-
+# remove build folder and create a clean one afterwards
+rm -rf $build_folder
+mkdir $build_folder
 
 # compile the file
 echo compiling as $filename...
@@ -98,21 +81,12 @@ fi
 if [ $d64 ]
 then
     echo creating d64 file
-    $vicepath/c1541 -format $filename,1 d64 $build_folder/$filename.d64 -write $build_folder/$filename.prg $filename
+    $path_vice/c1541 -format $filename,1 d64 $PWD/$build_folder/$filename.d64 -write $PWD/$build_folder/$filename.prg $filename
 fi
 
 # execute vice emulator
 if [ !$emulator ] || [ $emulator = "vice" ]
 then
     echo launching VICE
-    echo $path
     $path_vice/x64sc -moncommands $PWD/$build_folder/labels $PWD/$build_folder/$filename.prg
 fi
-
-# execute 64debugger emulator
-if [ $emulator = "64debugger" ]
-then
-    echo launching 64debugger
-    $path_64debugger -prg $build_folder/$filename.prg
-fi
-
