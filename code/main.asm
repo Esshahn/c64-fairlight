ba7957d3881f9d7d3f5902bf1aab698af333edca
Ingo Hinterding <ingo@hinterding.de>
2021-04-17 15:16:00 +0200

    stub fix

--- code/main.asm
+++ code/main.asm
@@ -7,7 +7,8 @@
 ; labels
 ;==========================================================
 
-entry       = $c000
+BASIC           = $0801
+CODE_START      = $c000
 
 
 ;==========================================================
@@ -19,11 +20,11 @@ entry       = $c000
     !byte $0b, $08
     !byte $E5                     ; BASIC line number:  $E2=2018 $E3=2019 etc       
     !byte $07, $9E
-    !byte '0' + entry % 100000 / 10000
-    !byte '0' + entry %  10000 / 1000        
-    !byte '0' + entry %   1000 /  100        
-    !byte '0' + entry %    100 /   10        
-    !byte '0' + entry %     10             
+    !byte '0' + CODE_START % 100000 / 10000
+    !byte '0' + CODE_START %  10000 /  1000        
+    !byte '0' + CODE_START %   1000 /   100        
+    !byte '0' + CODE_START %    100 /    10        
+    !byte '0' + CODE_START %     10             
     !byte $00, $00, $00           ; end of basic
 
 
